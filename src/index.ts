4bae184ebd69922c200e5b72bad8e0b5e26ad1a9
Randy Armstrong <randy@sparhawksoftware.com>
2024-09-29 00:44:40 -0700

    Update index file with constants.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@
 export * from './runtime';
 export * from './apis/index';
 export * from './models/index';
+export * from './opcua_attributes';
+export * from './opcua_statuscodes';
+export * from './opcua_constants';
